9fd57ac2d552e15d6b29dbddc4b7e302f71ce00d
AngelPortilla1 <Angelportihernan019@gmail.com>
2026-07-26 23:14:32 -0500

    Inicio limpio

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Conversor de temperatura")
+
+modo = st.radio("Convertir de:", ["Celsius a Fahrenheit", "Fahrenheit a Celsius"])
+valor = st.number_input("Valor")
+
+if modo == "Celsius a Fahrenheit":
+    resultado = valor * 9 / 5 + 32
+    st.write(f"{valor} °C son {round(resultado, 2)} °F")
+else:
+    resultado = (valor - 32) * 5 / 9
+    st.write(f"{valor} °F son {round(resultado, 2)} °C")
